cc950723f6a88655d8746d97489590c28dc787b6
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-04-20 18:56:11 -0300

    [CasoC] Terceiro commit
    Obter barra vermelha por novo teste para a mesma unidade previamente implementada
    
    Co-authored-by: marcelog5 <marcelovictorg2@gmail.com>
    Co-authored-by: Isabella <isabellacgmsa1@gmail.com>

--- src/test/java/com/example/project/RendimentoLiquidoTest.java
+++ src/test/java/com/example/project/RendimentoLiquidoTest.java
@@ -8,13 +8,25 @@ import org.junit.jupiter.api.Test;
 
 class RendimentoLiquidoTest {
     @Test
-    @DisplayName("Cálculo rendimento líquido")
+    @DisplayName("CI: R$ 4200.00\n IR: R$ 147.58\n RB: 983.84")
     void calcularRendimentoLiquido() {
         Calculator calculadora = new Calculator();
         double capitalInicial = 4200.00;
         double imposto = 147.58;
         double rendimentoBruto = 983.84;
         assertEquals(19.9110, 
+        calculadora.rendimentoLiquido(capitalInicial, imposto, rendimentoBruto),
+        0.00001);
+    }
+
+    @Test
+    @DisplayName("CI: R$ 1000,00\n IR: R$ 3,14\n RB: 13.97")
+    void calcularRendimentoLiquido2() {
+        Calculator calculadora = new Calculator();
+        double capitalInicial = 1000.00;
+        double imposto = 3.14;
+        double rendimentoBruto = 13.97;
+        assertEquals(1.0829, 
         calculadora.rendimentoLiquido(capitalInicial, imposto, rendimentoBruto),
         0.00001);
     }
